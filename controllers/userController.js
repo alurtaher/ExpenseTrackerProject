6a55b2639e6c1173ddb294c6cc27aaaa50cdc5e4
A Taher Basha <taherbashaengineer@gmail.com>
2025-07-29 18:05:37 +0530

    Task14: Forgot Password & Integrating Mailing Service

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,19 +2,19 @@ const path = require("path");
 const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const Sib = require("sib-api-v3-sdk");
+const dotenv = require("dotenv")
+dotenv.config();
 
 function generateAccessToken(id, email) {
-  return jwt.sign(
-    { userId: id, email: email },
-    "secret"
-  );
+  return jwt.sign({ userId: id, email: email }, "secret");
 }
 
 const isPremiumUser = (req, res, next) => {
   if (req.user.isPremiumUser) {
     return res.json({ isPremiumUser: true });
-  }else{
-    return res.json({isPremiumUser :false})
+  } else {
+    return res.json({ isPremiumUser: false });
   }
 };
 
@@ -87,10 +87,53 @@ const postUserLogin = (req, res, next) => {
   });
 };
 
+const resetPasswordPage = async (req, res, next) => {
+  try {
+    return res.status(200)
+      .sendFile(
+        path.join(__dirname, "../", "public", "views", "resetPassword.html")
+      );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const sendMail = async (req, res, next) => {
+  try {
+    const client = Sib.ApiClient.instance;
+    const apiKey = client.authentications["api-key"];
+    apiKey.apiKey = process.env.RESET_PASSWORD_API_KEY;
+    const transEmailApi = new Sib.TransactionalEmailsApi();
+    const sender = {
+      email: "taherbasha295@gmail.com",
+      name: "A Taher Basha",
+    };
+    const receivers = [
+      {
+        email: req.body.email,
+      },
+    ];
+    const emailResponse = await transEmailApi.sendTransacEmail({
+      sender,
+      to: receivers,
+      subject: "Expense Tracker Reset Password",
+      textContent: "Link Below",
+      // htmlContent: `<h3>link for reset the password</h3>`,
+    });
+    return res.send(
+      `<script>alert('Check your mails, Link for reset the password is successfully send on your Mail Id!'); window.location.href='/'</script>`
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 module.exports = {
   generateAccessToken,
   getLoginPage,
   postUserLogin,
   postUserSignUp,
   isPremiumUser,
+  resetPasswordPage,
+  sendMail,
 };
