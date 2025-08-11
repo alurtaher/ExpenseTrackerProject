46f9ac82b91583992dc0536b4643ff0a7e6d63a9
A Taher Basha <taherbashaengineer@gmail.com>
2025-08-11 13:24:19 +0530

    Error on mysql Solved

--- utils/database.js
+++ utils/database.js
@@ -1,12 +1,13 @@
 const Sequelize = require("sequelize");
-require('dotenv').config();
+require("dotenv").config();
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USERNAME,
   process.env.DB_PASSWORD,
   {
     dialect: "mysql",
-    host: "localhost",
+    host: process.env.DB_HOST || "localhost",
+    port: process.env.DB_PORT || 3306,
   }
 );
 
